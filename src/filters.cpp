f8556a6deb8be7326d8eaf55af03c7a31c43cd77
zmofei <13761509829@163.com>
2018-11-09 15:00:03 +0800

    pass property filter to filters.cpp

--- src/filters.cpp
+++ src/filters.cpp
@@ -7,12 +7,12 @@
 // clang-format off
 #include <node/src/node_conversion.hpp>
 // clang-format on
+#include <iostream>
 #include <mbgl/style/conversion.hpp>
 #include <mbgl/style/conversion/filter.hpp>
 #include <mbgl/style/filter.hpp>
 #include <string>
 #include <utility>
-#include <iostream>
 
 // Single constructor used for any new instance of it.
 Nan::Persistent<v8::FunctionTemplate>& Filters::constructor() {
@@ -157,40 +157,37 @@ NAN_METHOD(Filters::New) {
                     // insert the key/value into filters map
                     // TODO(dane): what if we have duplicate source-layer filters?
 
-                    // // handle property array
-                    // const v8::Local<v8::Value> layer_properties = layer->Get(Nan::New("properties").ToLocalChecked());
-                    // // error handling in case filter value passed in from JS-world is somehow invalid
+                    // handle property array
+                    const v8::Local<v8::Value> layer_properties = layer->Get(Nan::New("properties").ToLocalChecked());
+                    if (layer_properties->IsNull() || layer_properties->IsUndefined()) {
+                        Nan::ThrowError("Property-Filters is not properly constructed.");
+                        return;
+                    }
 
-                    // if (layer_properties->IsNull() || layer_properties->IsUndefined()) {
-                    //     Nan::ThrowError("Property-Filters is not properly constructed.");
-                    //     return;
-                    // }
+                    // NOTICE: If a layer is styled, but does not have a property, the property value will equal []
+                    // NOTICE: If a property is true, that means we need to keep all the properties
+                    filter_properties property;
+                    if (layer_properties->IsArray()) {
+                        auto propertyArray = layers_val.As<v8::Array>();
+                        uint32_t propertiesLength = propertyArray->Length();
+                        auto* proArray = new std::string[propertiesLength];
+                        for (uint32_t index = 0; index < length; ++index) {
+                            v8::Local<v8::Value> property_value = layers->Get(index);
+                            proArray[index] = *v8::String::Utf8Value(property_value->ToString());
 
-                    // // NOTICE: If a layer is styled, but does not have a property, the property value will equal []
-                    // // NOTICE: If a property is true, that means we need to keep all the properties
-                    // // If a boolean and is true, create a null/empty Filter object.
-                    // filter_properties property;
-                    // if (layer_properties->IsArray()) {
-                    //     auto propertyArray = layers_val.As<v8::Array>();
-                    //     length = propertyArray->Length();
-                    //     std::string* proArray = new std::string[length];
-                    //     for (i = 0; i < length; ++i) {
-                    //         v8::Local<v8::Value> property_value = layers->Get(i);
-                    //         v8::String::Utf8Value param1(property_value->ToString());
-                    //         proArray[i] = std::string(*param1);
-                    //         std::cout << proArray[i] << std::endl;
-                    //     }
-                    //     property = {"values", proArray};
-                    // } else if (layer_properties->IsBoolean() && layer_properties->IsTrue()) {
-                    //     property = {"all", new std::string[0]};
-                    // } else {
-                    //     Nan::ThrowTypeError("invalid filter value, must be an array or a boolean");
-                    //     return;
-                    // }
+                            std::cout << proArray[index] << std::endl;
+                        }
+                        property = {"values", proArray};
+                    } else if (layer_properties->IsBoolean() && layer_properties->IsTrue()) {
+                        property = {"all", new std::string[0]};
+                    } else {
+                        Nan::ThrowTypeError("invalid filter value, must be an array or a boolean");
+                        return;
+                    }
 
                     std::string source_layer = *v8::String::Utf8Value(layer_name->ToString());
 
-                    self->add_filter(std::move(source_layer), std::move(filter), {"values"}, minzoom, maxzoom);
+                    self->add_filter(std::move(source_layer), std::move(filter), property, minzoom, maxzoom);
                     // can find these via filters.find("x")->second
                 }
             }
