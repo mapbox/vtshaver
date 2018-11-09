25485d09ad115f8674f15bed93748fdf8a29e78c
zmofei <13761509829@163.com>
2018-11-09 14:38:49 +0800

    pass property filter to filters.cpp

--- src/filters.hpp
+++ src/filters.hpp
@@ -10,9 +10,13 @@
 class Filters : public Nan::ObjectWrap {
   public:
     using filter_value_type = mbgl::style::Filter;
+    using filter_properties = struct {
+        std::string type; // [value:"values"| all:"keep all the values"]
+        std::string* value;
+    };
     using filter_key_type = std::string; // TODO: convert to data_view
     using zoom_type = double;
-    using filter_values_type = std::tuple<filter_value_type, zoom_type, zoom_type>;
+    using filter_values_type = std::tuple<filter_value_type, filter_properties, zoom_type, zoom_type>;
     using filters_type = std::map<filter_key_type, filter_values_type>;
 
     // initializer
@@ -23,9 +27,9 @@ class Filters : public Nan::ObjectWrap {
 
     static Nan::Persistent<v8::FunctionTemplate>& constructor();
 
-    void add_filter(filter_key_type&& key, filter_value_type&& filter, zoom_type minzoom, zoom_type maxzoom) {
+    void add_filter(filter_key_type&& key, filter_value_type&& filter, filter_properties properties, zoom_type minzoom, zoom_type maxzoom) {
         // add a new key/value pair, with the value equaling a tuple 'filter_values_type' defined above
-        filters.emplace(key, std::make_tuple(std::move(filter), minzoom, maxzoom));
+        filters.emplace(key, std::make_tuple(std::move(filter), properties, minzoom, maxzoom));
     }
 
     filters_type const& get_filters() const {
