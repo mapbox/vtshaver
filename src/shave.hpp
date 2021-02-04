b414a8d4bf6a45bb68adfcd9033b21d713b4dec6
Artem Pavlenko <artem@mapnik.org>
2021-02-04 09:24:49 +0000

    Remove unused declarations + cleanup

--- src/shave.hpp
+++ src/shave.hpp
@@ -1,9 +1,6 @@
 #pragma once
 
 #include <napi.h>
-#include <uv.h>
 
 // shave, custom async method
-Napi::Value shave(const Napi::CallbackInfo& info);
-void AsyncShave(uv_work_t* req);
-void AfterShave(uv_work_t* req);
+Napi::Value shave(Napi::CallbackInfo const& info);
